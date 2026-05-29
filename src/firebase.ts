1ffdd6b58920fe8a038cf3c0254ae6ae7e6a71ea
NMD <delacruznd@students.nu-moa.edu.ph>
2026-05-29 20:21:59 +0800

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -13,7 +13,7 @@ import { getFirestore, doc, getDocFromServer } from 'firebase/firestore';
 import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
 export const auth = getAuth();
 
 export {
